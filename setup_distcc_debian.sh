795701fe84ae13a976b325d2f9b9c1f5c7a49231
Gary Johnson <gary@gjtt.com>
2014-11-27 09:17:02 -0800

    Adding first run of scripts

---
+++ setup_distcc_debian.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+read -s -p "Enter password for distcc user (put it somewhere safe!): " PASSWORD
+
+useradd -m distcc
+echo "distcc:$PASSWORD" | chpasswd
+
+sudo adduser distcc sudo
+su distcc <<EOSU
+echo $PASSWORD | sudo -S apt-get update
+echo $PASSWORD | sudo -S apt-get install util-linux gnu-fdisk git net-tools git-core subversion autoconf automake python python-dev libgtk2.0-dev binutils-dev ntp libc6-i386 lib32z1 lib32stdc++6 -y
+
+distcc_dir = mktemp -d
+cd $distcc_dir
+svn checkout http://distcc.googlecode.com/svn/trunk/ distcc-read-only
+cd $distcc_dir/distcc-read-only
+./autogen.sh
+./configure --with-gtk
+make
+echo $PASSWORD | sudo -S make install
+rm -r $distcc_dir
+
+cd ~
+git clone https://github.com/raspberrypi/tools.git --depth=1
+cd ~/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin
+echo $PASSWORD | sudo -S ln -sf arm-linux-gnueabihf-gcc gcc
+echo $PASSWORD | sudo -S ln -sf arm-linux-gnueabihf-gcc cc
+echo $PASSWORD | sudo -S ln -sf arm-linux-gnueabihf-c++ c++
+echo $PASSWORD | sudo -S ln -sf arm-linux-gnueabihf-cpp cpp
+echo $PASSWORD | sudo -S ln -sf arm-linux-gnueabihf-g++ g++
+EOSU
---
+++ setup_distcc_raspbian.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+DISTCC_USER = distcc
+read -s -p "Enter password for sudo commands: " PASSWORD
+read -s "Enter IP address for build executor: " BUILD_IP
+read -s -p "Enter password for $DISTCC_USER user on build executor: " DISTCC_PASSWORD
+
+echo $PASSWORD | sudo -S apt-get update
+echo $PASSWORD | sudo -S apt-get install sshpass util-linux gnu-fdisk git net-tools git-core subversion autoconf automake python python-dev libgtk2.0-dev binutils-dev ntp -y
+
+libiberty_dir = mktemp -d
+cd $libiberty_dir
+wget https://toolbox-of-eric.googlecode.com/files/libiberty.tar.gz
+tar -xzvf libibterty.tar.gz
+cd $libiberty_dir/libiberty
+echo $PASSWORD | sudo -S ./configure
+echo $PASSWORD | sudo -S make
+echo $PASSWORD | sudo -S make install
+rm -r $libiberty_dir
+
+distcc_dir = mktemp -d
+cd $distcc_dir
+svn checkout http://distcc.googlecode.com/svn/trunk/ distcc-read-only
+cd $distcc_dir/distcc-read-only
+./autogen.sh
+./configure --with-gtk
+make
+echo $PASSWORD | sudo -S make install
+rm -r $distcc_dir
+
+echo $PASSWORD | sudo -S ln -s /usr/local/bin/distcc /usr/local/bin/gcc
+echo $PASSWORD | sudo -S ln -s /usr/local/bin/distcc /usr/local/bin/cc
+echo $PASSWORD | sudo -S ln -s /usr/local/bin/distcc /usr/local/bin/g++
+echo $PASSWORD | sudo -S ln -s /usr/local/bin/distcc /usr/local/bin/c++
+echo $PASSWORD | sudo -S ln -s /usr/local/bin/distcc /usr/local/bin/cpp
+
+ssh-keygen -t rsa -C distcc_build -f ~/.ssh/id_rsa_distcc_build -q -P ""
+sshpass -p $DISTCC_PASSWORD scp ~/.ssh/id_rsa_distcc_build.pub $DISTCC_USER@$BUILD_IP:/home/$DISTCC_USER/.ssh/authorized_keys_distcc_build
+
+echo DISTCC_HOSTS="$DISTCC_USER@$BUILD_IP/16" >> ~/.bash_profile
+echo DISTCC_BACKOFF_PERIOD=0 >> ~/.bash_profile
+echo DISTCC_IO_TIMEOUT=3000 >> ~/.bash_profile
+echo DISTCC_SKIP_LOCAL_RETRY=1 >> ~/.bash_profile
+echo PATH=/usr/local/bin:$PATH >> ~/.bash_profile
+# can we get this over to the other machine?
+echo DISTCC_PATH=/home/$DISTCC_USER/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin:/home/$DISTCC_USER/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/arm-linux-gnueabihf/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games >> ~/.bash_profile
